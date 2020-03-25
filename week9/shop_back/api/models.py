f96b9af82fb674270bd1493391e6fa735a49888f
Anna <renita5701@gmail.com>
2020-03-25 09:05:41 +0600

    upd

--- week9/shop_back/api/models.py
+++ week9/shop_back/api/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=300)
     price = models.FloatField()
-    description = models.TextField()
+    description = models.TextField(default='')
     count = models.IntegerField()
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count
+        }
 
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=300)
--- week9/shop_back/api/views.py
+++ week9/shop_back/api/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
+from api.models import Product, Category
+from django.http import JsonResponse
 
 
-def products_list():
+def products_list(request):
+    products = Product.objects.all()
+    products_json = [product.to_json() for product in products]
+    return JsonResponse(products_json, safe=False)
+
+
+def categories_list(request):
     pass
 
 
-def categories_list():
+def category_detail(request):
     pass
 
 
-def category_detail():
-    pass
-
-
-def category_products():
+def category_products(request):
     pass
