2675fc59b74ad4d2b8ded316cf3ecb241c02ed4d
Anna <renita5701@gmail.com>
2020-03-26 11:47:14 +0600

    upd

--- week9/shop_back/api/models.py
+++ week9/shop_back/api/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    def to_json(self):
+        return {
+            'name': self.name
+        }
+
+
 class Product(models.Model):
     name = models.CharField(max_length=300)
     price = models.FloatField()
     description = models.TextField(default='')
     count = models.IntegerField()
+    category = models.ForeignKey(Category)
 
     def to_json(self):
         return {
@@ -13,13 +23,4 @@ class Product(models.Model):
             'price': self.price,
             'description': self.description,
             'count': self.count
-        }
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=300)
-
-    def to_json(self):
-        return {
-            'name': self.name
         }
