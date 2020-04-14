65053641615ebf4d67930c1d15800fcc43e47099
Anna <renita5701@gmail.com>
2020-04-14 10:11:42 +0600

    mixins

--- week13/hh_back/api/urls.py
+++ week13/hh_back/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from api.views import companies_list, companies_detail, company_vacancies, vacancies_list, vacancies_detail,\
-    vacancies_top
-from api.views.views_cbv import CompanyListAPIView, CompanyDetailAPIView, CompanyVacanciesListAPIView, \
-    VacancyListAPIView, VacancyDetailAPIView, TopVacanciesListAPIView
+# from api.views import companies_list, companies_detail, company_vacancies, vacancies_list, vacancies_detail,\
+#     vacancies_top
+# from api.views.views_cbv import CompanyListAPIView, CompanyDetailAPIView, CompanyVacanciesListAPIView, \
+#     VacancyListAPIView, VacancyDetailAPIView, TopVacanciesListAPIView
+from api.views.views_generic_v1 import CompanyListAPIView, CompanyDetailAPIView
 
 
 urlpatterns = [
@@ -16,8 +17,8 @@ urlpatterns = [
 
     path('companies/', CompanyListAPIView.as_view()),
     path('companies/<int:company_id>/', CompanyDetailAPIView.as_view()),
-    path('companies/<int:company_id>/vacancies/', CompanyVacanciesListAPIView.as_view()),
-    path('vacancies/', VacancyListAPIView.as_view()),
-    path('vacancies/<int:vacancy_id>/', VacancyDetailAPIView.as_view()),
-    path('vacancies/top_ten/', TopVacanciesListAPIView.as_view())
+    # path('companies/<int:company_id>/vacancies/', CompanyVacanciesListAPIView.as_view()),
+    # path('vacancies/', VacancyListAPIView.as_view()),
+    # path('vacancies/<int:vacancy_id>/', VacancyDetailAPIView.as_view()),
+    # path('vacancies/top_ten/', TopVacanciesListAPIView.as_view())
 ]
---
+++ week13/hh_back/api/views/views_generic_v1.py
@@ -0,0 +1,36 @@
+from rest_framework import generics
+from rest_framework import mixins
+
+from api.models import Company
+from api.serializers import CompanySerializer2
+
+
+class CompanyListAPIView(mixins.ListModelMixin,
+                         mixins.CreateModelMixin,
+                         generics.GenericAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer2
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(self, request, *args, **kwargs)
+
+
+class CompanyDetailAPIView(mixins.RetrieveModelMixin,
+                           mixins.UpdateModelMixin,
+                           mixins.DestroyModelMixin,
+                           generics.GenericAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer2
+    lookup_url_kwarg = 'company_id'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
